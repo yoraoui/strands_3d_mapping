76e3129b6f93526bb4bed341c9fd2dbb11bbe77d
jekekrantz <jekekrantz@github.com>
2016-08-18 22:22:54 +0200

    working on additional view registration and segmentation

--- semantic_map/src/semantic_map_recompute.cpp
+++ semantic_map/src/semantic_map_recompute.cpp
@@ -333,6 +333,118 @@ void fixXml(std::string sweep_xml){
 	delete xmlWriter;
 }
 
+std::string outtopic = "/some/topic";
+ros::Publisher out_pub;
+void processMetaroom(std::string path){
+    printf("processing: %s\n",path.c_str());
+
+    int slash_pos = path.find_last_of("/");
+    std::string sweep_folder = path.substr(0, slash_pos) + "/";
+
+    std::vector<cv::Mat> viewrgbs;
+    std::vector<cv::Mat> viewdepths;
+    std::vector<tf::StampedTransform > viewtfs;
+
+    QStringList objectFiles = QDir(sweep_folder.c_str()).entryList(QStringList("*object*.xml"));
+    for (auto objectFile : objectFiles){
+        auto object = loadDynamicObjectFromSingleSweep<PointType>(sweep_folder+objectFile.toStdString(),false);
+        for (unsigned int i=0; i<object.vAdditionalViews.size(); i++){
+
+            CloudPtr cloud = object.vAdditionalViews[i];
+
+
+            cv::Mat rgb;
+            rgb.create(cloud->height,cloud->width,CV_8UC3);
+            unsigned char * rgbdata = (unsigned char *)rgb.data;
+
+            cv::Mat depth;
+            depth.create(cloud->height,cloud->width,CV_16UC1);
+            unsigned short * depthdata = (unsigned short *)depth.data;
+
+            unsigned int nr_data = cloud->height * cloud->width;
+            for(unsigned int j = 0; j < nr_data; j++){
+                PointType p = cloud->points[j];
+                rgbdata[3*j+0]	= p.b;
+                rgbdata[3*j+1]	= p.g;
+                rgbdata[3*j+2]	= p.r;
+                depthdata[j]	= short(5000.0 * p.z);
+            }
+
+            viewrgbs.push_back(rgb);
+            viewdepths.push_back(depth);
+            viewtfs.push_back(object.vAdditionalViewsTransforms[i]);
+            //viewcams.push_back(roomData.vIntermediateRoomCloudCamParams.front());
+
+            cv::namedWindow("rgbimage",     cv::WINDOW_AUTOSIZE);
+            cv::imshow(		"rgbimage",     rgb);
+            cv::namedWindow("depthimage",	cv::WINDOW_AUTOSIZE);
+            cv::imshow(		"depthimage",	depth);
+            cv::waitKey(30);
+
+        }
+    }
+
+    std::vector<Eigen::Matrix4d> unrefined;
+    std::vector<double> times;
+    for(unsigned int i = 0; i < viewrgbs.size(); i++){
+        geometry_msgs::TransformStamped msg;
+        tf::transformStampedTFToMsg(viewtfs[i], msg);
+        long sec = msg.header.stamp.sec;
+        long nsec = msg.header.stamp.nsec;
+        double time = double(sec)+1e-9*double(nsec);
+
+        Eigen::Quaterniond q;
+        q.w() = msg.transform.rotation.w;
+        q.x() = msg.transform.rotation.x;
+        q.y() = msg.transform.rotation.y;
+        q.z() = msg.transform.rotation.z;
+
+        Eigen::Affine3d a (q);
+        a(0,3) = msg.transform.translation.x;
+        a(1,3) = msg.transform.translation.y;
+        a(2,3) = msg.transform.translation.z;
+
+
+        unrefined.push_back(a.matrix());
+        times.push_back(time);
+    }
+
+/*
+    xmlWriter->writeStartElement("Transform");
+    xmlWriter->writeStartElement("Translation");
+    xmlWriter->writeStartElement("x");
+    xmlWriter->writeCharacters(QString::number(msg.transform.translation.x));
+    xmlWriter->writeEndElement();
+    xmlWriter->writeStartElement("y");
+    xmlWriter->writeCharacters(QString::number(msg.transform.translation.y));
+    xmlWriter->writeEndElement();
+    xmlWriter->writeStartElement("z");
+    xmlWriter->writeCharacters(QString::number(msg.transform.translation.z));
+    xmlWriter->writeEndElement();
+    xmlWriter->writeEndElement(); // Translation
+    xmlWriter->writeStartElement("Rotation");
+    xmlWriter->writeStartElement("w");
+    xmlWriter->writeCharacters(QString::number(msg.transform.rotation.w));
+    xmlWriter->writeEndElement();
+    xmlWriter->writeStartElement("x");
+    xmlWriter->writeCharacters(QString::number(msg.transform.rotation.x));
+    xmlWriter->writeEndElement();
+    xmlWriter->writeStartElement("y");
+    xmlWriter->writeCharacters(QString::number(msg.transform.rotation.y));
+    xmlWriter->writeEndElement();
+    xmlWriter->writeStartElement("z");
+    xmlWriter->writeCharacters(QString::number(msg.transform.rotation.z));
+*/
+    std_msgs::String msg;
+    msg.data = path;
+    out_pub.publish(msg);
+    ros::spinOnce();
+}
+
+void chatterCallback(const std_msgs::String::ConstPtr& msg){
+    processMetaroom(msg->data);
+}
+
 void segmentWithAdditionalViewsXml(std::string sweep_xml,std::string prev){
 	printf("segmentWithAdditionalViewsXml\n");
 //	std::string prev = getPrevXML(sweep_xml);
@@ -341,37 +453,66 @@ void segmentWithAdditionalViewsXml(std::string sweep_xml,std::string prev){
 
 int main(int argc, char **argv)
 {
-	for(int ar = 1; ar < argc; ar++){
-		std::string overall_folder = std::string(argv[ar]);
-		printf("overall folder: %s\n",overall_folder.c_str());
+    ros::init(argc, argv, "metaroom_additional_view_processing");
+    ros::NodeHandle n;
 
-		std::vector<std::string> sweep_xmls = semantic_map_load_utilties::getSweepXmls<pcl::PointXYZRGB>(overall_folder);
-		for (int i = 0; i < sweep_xmls.size(); i++){//auto sweep_xml : sweep_xmls) {
-			printf("current xml: %s\n",sweep_xmls[i].c_str());
-			fixXml(sweep_xmls[i]);
-			if(i > 1){
-				segmentWithAdditionalViewsXml(sweep_xmls[i-1],sweep_xmls[i]);
-			}
-		}
-	}
 
-	printf("test\n");
-	exit(0);
-	// Set up ROS.
-	ros::init(argc, argv, "Semantic_map_node");
-	ros::NodeHandle n;
+    int inputstate = 0;
+    for(int i = 1; i < argc;i++){
+        printf("input: %s\n",argv[i]);
+        if(std::string(argv[i]).compare("-file") == 0){inputstate = 2;}
+        else if(std::string(argv[i]).compare("-intopic") == 0){inputstate = 0;}
+        else if(std::string(argv[i]).compare("-outtopic") == 0){inputstate = 1;}
+        else if(inputstate == 0){
+            out_pub = n.advertise<std_msgs::String>(outtopic, 1000);
+            ros::Subscriber sub = n.subscribe(std::string(argv[i]), 1000, chatterCallback);
+            ros::spin();
+        }else if(inputstate == 1){
+            outtopic = std::string(argv[i]);
+        }else{
 
-	ros::NodeHandle aRosNode("~");
-	printf("test\n");
-	//SemanticMapNode<pcl::PointXYZRGB> aSemanticMapNode(aRosNode);
-	SemanticMapNode<pcl::PointXYZRGB> aSemanticMapNode(n);
-	aSemanticMapNode.processRoomObservation(argv[1]);
-	exit(0);
-	printf("test\n");
-	ros::Rate loop_rate(10);
-	while (ros::ok())
-	{
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
+            out_pub = n.advertise<std_msgs::String>(outtopic, 1000);
+            processMetaroom(std::string(argv[i]));
+        }
+    }
+
+//    std::string listentopic = "/some/stupid/topic";
+//    if(argc == 2){listentopic = std::string(argv[1]);}
+//    else{printf("robot listener: incorrect number of args. Should take a single arguement with a topicname!\n");}
+
+
+
+//	for(int ar = 1; ar < argc; ar++){
+//		std::string overall_folder = std::string(argv[ar]);
+//		printf("overall folder: %s\n",overall_folder.c_str());
+
+//		std::vector<std::string> sweep_xmls = semantic_map_load_utilties::getSweepXmls<pcl::PointXYZRGB>(overall_folder);
+//		for (int i = 0; i < sweep_xmls.size(); i++){//auto sweep_xml : sweep_xmls) {
+//			printf("current xml: %s\n",sweep_xmls[i].c_str());
+//			fixXml(sweep_xmls[i]);
+//			if(i > 1){
+//				segmentWithAdditionalViewsXml(sweep_xmls[i-1],sweep_xmls[i]);
+//			}
+//		}
+//	}
+
+//	printf("test\n");
+//	exit(0);
+//	// Set up ROS.
+//	ros::init(argc, argv, "Semantic_map_node");
+//	ros::NodeHandle n;
+
+//	ros::NodeHandle aRosNode("~");
+//	printf("test\n");
+//	//SemanticMapNode<pcl::PointXYZRGB> aSemanticMapNode(aRosNode);
+//	SemanticMapNode<pcl::PointXYZRGB> aSemanticMapNode(n);
+//	aSemanticMapNode.processRoomObservation(argv[1]);
+//	exit(0);
+//	printf("test\n");
+//	ros::Rate loop_rate(10);
+//	while (ros::ok())
+//	{
+//		ros::spinOnce();
+//		loop_rate.sleep();
+//	}
 }
