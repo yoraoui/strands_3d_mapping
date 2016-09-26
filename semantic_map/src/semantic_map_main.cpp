77379dc2e2785d482a91f25773716ba2b9a440ce
jekekrantz <johan.ekekrantz@gmail.com>
2016-09-26 12:01:37 +0200

    fixing semantic map integration with segmentationsever

--- semantic_map/src/semantic_map_main.cpp
+++ semantic_map/src/semantic_map_main.cpp
@@ -2,18 +2,13 @@
 
 int main(int argc, char **argv)
 {
-    printf("test\n");
-    // Set up ROS.
     ros::init(argc, argv, "Semantic_map_node");
     ros::NodeHandle n;
 
     ros::NodeHandle aRosNode("~");
-    printf("test\n");
-	//SemanticMapNode<pcl::PointXYZRGB> aSemanticMapNode(aRosNode);
 	SemanticMapNode<pcl::PointXYZRGB> aSemanticMapNode(n);
     aSemanticMapNode.processRoomObservation(argv[1]);
-    exit(0);
-    printf("test\n");
+
     ros::Rate loop_rate(10);
     while (ros::ok())
     {
