37d895b545eaee03c0e946ee14dfec0e978668fa
jekekrantz <johan.ekekrantz@gmail.com>
2016-10-04 21:25:52 +0200

    ...

--- learn_objects_action/src/learn_objects_action/metric_sweep.py
+++ learn_objects_action/src/learn_objects_action/metric_sweep.py
@@ -126,8 +126,7 @@ class SelectCluster(smach.State):
         self._debug_services = debug_services
 
 
-    def execute(self, userdata):
-		print "SelectCluster::execute"
+	def execute(self, userdata):
         try:
             # Load the waypoint to soma from file
             if self._rois_file != "NONE":
@@ -138,10 +137,7 @@ class SelectCluster(smach.State):
                 soma_region = ""
 
             #clusters = self._get_clusters(userdata['waypoint'])
-            clusters = self._get_clusters(userdata.action_goal.waypoint)
-
-
-			print "Line 1"
+			clusters = self._get_clusters(userdata.action_goal.waypoint)
 
             if self._debug_mode:
                 available=[str(s) for s in range(len(clusters.object_id))]
@@ -149,8 +145,6 @@ class SelectCluster(smach.State):
                 select=self._debug_services.get_proceed(("none","auto") + tuple(available))
             else:
                 select="auto"
-
-			print "Line 2"
 
             if select=="PREEMPT" or select=="none":
                 return 'none'
@@ -174,9 +168,6 @@ class SelectCluster(smach.State):
                 ID=numpy.argmax(scores) # the one to look at is the one that has the most observation available
             else:
                 ID=int(select)
-
-
-			print "Line 3"
 
             rospy.logwarn( "Getting cluster: %s"%clusters.object_id[ID])
             #one_cluster = self._get_specific_cluster(userdata['waypoint'], clusters.object_id[ID])
