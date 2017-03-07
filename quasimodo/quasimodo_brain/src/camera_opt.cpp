cec0d2291bef0c740f3f6e6c96a2d0c66a98a543
jekekrantz <johan.ekekrantz@gmail.com>
2017-03-07 17:03:21 +0100

    pixel reweighter working somewhat

--- quasimodo/quasimodo_brain/src/camera_opt.cpp
+++ quasimodo/quasimodo_brain/src/camera_opt.cpp
@@ -5,9 +5,11 @@ class CameraOptimizer{
 public:
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
     int visualizationLvl;
+    int z_slider;
 
     CameraOptimizer(){
         visualizationLvl = 0;
+        z_slider = 1000;
     }
     ~CameraOptimizer(){}
 
@@ -67,24 +69,207 @@ public:
 	}
 
 	virtual void addConstraint(double w, double h, double z, double z2, double weight){
-
+        printf("old addConstraint\n");
 	}
+
+
+    virtual void show(bool stop = false){
+        cv::namedWindow(    "Scaling");
+        cv::createTrackbar( "Scaling", "z", &z_slider, 10000);
+
+		int width = 640;
+		int height = 480;
+
+        cv::Mat img;
+        img.create(height,width,CV_8UC3);
+        unsigned char * imgdata   = img.data;
+
+        double z = double(z_slider)*0.001;
+        double z_max = 0;
+        double z_min = 10000;
+        std::vector<double> z_ratios;
+        z_ratios.resize(width*height);
+
+		for(unsigned long h = 0; h < height; h++){
+			for(unsigned long w = 0; w < width; w++){
+                unsigned int ind = h * width + w;
+				z_ratios[ind] = getRange(double(w)/double(width), double(h)/double(height),z)/z;
+                z_max = std::max(z_ratios[ind],z_max);
+                z_min = std::min(z_ratios[ind],z_min);
+            }
+        }
+
+		double z_range = std::max(0.000001,z_max - z_min);
+
+		for(unsigned long h = 0; h < height; h++){
+			for(unsigned long w = 0; w < width; w++){
+                unsigned int ind = h * width + w;
+                double zr = (z_ratios[ind]-z_min)/z_range;
+                imgdata[3*ind+0] = 255*zr;
+                imgdata[3*ind+1] = 0;
+                imgdata[3*ind+2] = 255*(1-zr);
+            }
+        }
+
+        char buf [1024];
+		sprintf(buf,"max: %7.7f min: %7.7f",z_max,z_min);
+
+        cv::putText(img,std::string(buf),cv::Point(50,30), CV_FONT_HERSHEY_SIMPLEX, 1.0,cv::Scalar(0,255,0),2,8,false);
+
+        cv::imshow( "Scaling",	img );
+		if(stop){	cv::waitKey(0);}
+		else{		cv::waitKey(30);}
+    }
+
+	virtual void show(reglib::RGBDFrame * src, reglib::RGBDFrame * dst, Eigen::Matrix4d p){
+		printf("show\n");
+		viewer->removeAllPointClouds();
+		float m00 = p(0,0); float m01 = p(0,1); float m02 = p(0,2); float m03 = p(0,3);
+		float m10 = p(1,0); float m11 = p(1,1); float m12 = p(1,2); float m13 = p(1,3);
+		float m20 = p(2,0); float m21 = p(2,1); float m22 = p(2,2); float m23 = p(2,3);
+
+		unsigned short * dst_depthdata		= (unsigned short	*)(dst->depth.data);
+
+		reglib::Camera * dst_camera		= dst->camera;
+		const unsigned int dst_width	= dst_camera->width;
+		const unsigned int dst_height	= dst_camera->height;
+		const float dst_idepth			= dst_camera->idepth_scale;
+		const float dst_cx				= dst_camera->cx;
+		const float dst_cy				= dst_camera->cy;
+		const float dst_fx				= dst_camera->fx;
+		const float dst_fy				= dst_camera->fy;
+		const float dst_ifx				= 1.0/dst_camera->fx;
+		const float dst_ify				= 1.0/dst_camera->fy;
+		const unsigned int dst_width2	= dst_camera->width  - 2;
+		const unsigned int dst_height2	= dst_camera->height - 2;
+
+		unsigned short * src_depthdata		= (unsigned short	*)(src->depth.data);
+
+		reglib::Camera * src_camera				= src->camera;
+		const unsigned int src_width	= src_camera->width;
+		const unsigned int src_height	= src_camera->height;
+		const float src_idepth			= src_camera->idepth_scale;
+		const float src_cx				= src_camera->cx;
+		const float src_cy				= src_camera->cy;
+		const float src_ifx				= 1.0/src_camera->fx;
+		const float src_ify				= 1.0/src_camera->fy;
+
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr src_before (new pcl::PointCloud<pcl::PointXYZRGB> ());
+		for(unsigned long h = 0; h < src_height;h ++){
+			for(unsigned long w = 0; w < src_width;w ++){
+				float z = src_idepth*float(src_depthdata[h * src_width + w]);
+				if(z > 0){
+					float x     = (float(w) - src_cx) * z * src_ifx;
+					float y     = (float(h) - src_cy) * z * src_ify;
+
+					pcl::PointXYZRGB p;
+					p.x	= m00*x + m01*y + m02*z + m03;
+					p.y	= m10*x + m11*y + m12*z + m13;
+					p.z	= m20*x + m21*y + m22*z + m23;
+					p.r = 0;
+					p.g = 255;
+					p.b = 0;
+					src_before->points.push_back(p);
+				}
+			}
+		}
+
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr dst_before (new pcl::PointCloud<pcl::PointXYZRGB> ());
+		for(unsigned long h = 0; h < dst_height;h ++){
+			for(unsigned long w = 0; w < dst_width;w ++){
+				float z = dst_idepth*float(dst_depthdata[h * dst_width + w]);
+				if(z > 0){
+					float x     = (float(w) - dst_cx) * z * dst_ifx;
+					float y     = (float(h) - dst_cy) * z * dst_ify;
+
+					pcl::PointXYZRGB p;
+					p.x	= x;
+					p.y	= y;
+					p.z	= z;
+					p.r = 255;
+					p.g = 0;
+					p.b = 0;
+					dst_before->points.push_back(p);
+				}
+			}
+		}
+
+
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr src_after (new pcl::PointCloud<pcl::PointXYZRGB> ());
+		for(unsigned long h = 0; h < src_height;h ++){
+			for(unsigned long w = 0; w < src_width;w ++){
+				float z = src_idepth*float(src_depthdata[h * src_width + w]);
+				if(z > 0){
+					z = getRange(float(w)/float(src_width),float(h)/float(src_height),z);
+					float x     = (float(w) - src_cx) * z * src_ifx;
+					float y     = (float(h) - src_cy) * z * src_ify;
+
+					pcl::PointXYZRGB p;
+					p.x	= m00*x + m01*y + m02*z + m03;
+					p.y	= m10*x + m11*y + m12*z + m13;
+					p.z	= m20*x + m21*y + m22*z + m23;
+					p.r = 0;
+					p.g = 255;
+					p.b = 0;
+					p.x += 3;
+					src_after->points.push_back(p);
+				}
+			}
+		}
+
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr dst_after (new pcl::PointCloud<pcl::PointXYZRGB> ());
+		for(unsigned long h = 0; h < dst_height;h ++){
+			for(unsigned long w = 0; w < dst_width;w ++){
+				float z = dst_idepth*float(dst_depthdata[h * dst_width + w]);
+				if(z > 0){
+					z = getRange(float(w)/float(dst_width),float(h)/float(dst_height),z);
+					float x     = (float(w) - dst_cx) * z * dst_ifx;
+					float y     = (float(h) - dst_cy) * z * dst_ify;
+
+					pcl::PointXYZRGB p;
+					p.x	= x;
+					p.y	= y;
+					p.z	= z;
+					p.r = 255;
+					p.g = 0;
+					p.b = 0;
+					p.x += 3;
+					dst_after->points.push_back(p);
+				}
+			}
+		}
+
+
+
+		viewer->addPointCloud<pcl::PointXYZRGB> (src_before, pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>(src_before), "src_before");
+		viewer->addPointCloud<pcl::PointXYZRGB> (dst_before, pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>(dst_before), "dst_before");
+		viewer->addPointCloud<pcl::PointXYZRGB> (src_after, pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>(src_after), "src_after");
+		viewer->addPointCloud<pcl::PointXYZRGB> (dst_after, pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>(dst_after), "dst_after");
+		viewer->spin();
+		viewer->removeAllPointClouds();
+	}
+
+
+
+	virtual void normalize(){}
+
     virtual double getRange(double w, double h, double z){return z;}
 
     virtual void addTrainingData( reglib::RGBDFrame * src, reglib::RGBDFrame * dst, Eigen::Matrix4d p){
-
+		double start1 = reglib::getTime();
 		cv::Mat src_reliability = getPixelReliability(src);
 		float *  src_rel = (float*)src_reliability.data;
 		cv::Mat dst_reliability = getPixelReliability(dst);
 		float *  dst_rel = (float*)dst_reliability.data;
+		double stop1 = reglib::getTime();
+
+		double start2 = reglib::getTime();
 
         float m00 = p(0,0); float m01 = p(0,1); float m02 = p(0,2); float m03 = p(0,3);
         float m10 = p(1,0); float m11 = p(1,1); float m12 = p(1,2); float m13 = p(1,3);
         float m20 = p(2,0); float m21 = p(2,1); float m22 = p(2,2); float m23 = p(2,3);
 
-        unsigned char  * dst_detdata		= (unsigned char	*)(dst->det_dilate.data);
-        unsigned short * dst_depthdata		= (unsigned short	*)(dst->depth.data);
-        float		   * dst_normalsdata	= (float			*)(dst->normals.data);
+		unsigned short * dst_depthdata		= (unsigned short	*)(dst->depth.data);
 
 		reglib::Camera * dst_camera		= dst->camera;
         const unsigned int dst_width	= dst_camera->width;
@@ -110,21 +295,14 @@ public:
         const float src_ifx				= 1.0/src_camera->fx;
         const float src_ify				= 1.0/src_camera->fy;
 
-        cv::Mat rz;
-        rz.create(src_height,src_width,CV_8UC3);
-        unsigned char   * rzdata   = rz.data;
-
         for(unsigned long src_h = 0; src_h < src_height;src_h ++){
             for(unsigned long src_w = 0; src_w < src_width;src_w ++){
                 unsigned int src_ind = src_h * src_width + src_w;
-
-                rzdata[3*src_ind+0] = 0;
-                rzdata[3*src_ind+1] = 0;
-				rzdata[3*src_ind+2] = 0;
 				double sr = src_rel[src_ind];
 
 				if(sr == 0){continue;}
-				float z         = getRange(src_w,src_h,src_idepth*float(src_depthdata[src_ind]));
+
+				float z = getRange(src_w/float(src_width),src_h/float(src_height),src_idepth*float(src_depthdata[src_ind]));//src_idepth*float(src_depthdata[src_ind]);//getRange(src_w/float(src_width),src_h/float(src_height),src_idepth*float(src_depthdata[src_ind]));
 
 				if(z > 0){
                     float x     = (float(src_w) - src_cx) * z * src_ifx;
@@ -133,7 +311,6 @@ public:
                     float src_tx	= m00*x + m01*y + m02*z + m03;
                     float src_ty	= m10*x + m11*y + m12*z + m13;
                     float src_tz	= m20*x + m21*y + m22*z + m23;
-
 
                     float itz	= 1.0/src_tz;
                     float dst_w	= dst_fx*src_tx*itz + dst_cx;
@@ -147,40 +324,23 @@ public:
                         float dst_z = dst_idepth*float(dst_depthdata[dst_ind]);
 						if(dst_z > 0){
 							double src_cov = z*z*z*z;
-							double src_info = 1.0 / src_info;
+//							double src_info = 1.0 / src_info;
 							double dst_cov = dst_z*dst_z*dst_z*dst_z;
-							double dst_info = 1.0 /dst_cov;
+//							double dst_info = 1.0 /dst_cov;
 							double total_cov = src_cov + dst_cov;
-							double total_std = sqrt(total_cov);
-							double scale = 1.0/total_std;
-                            double dz = dst_z-src_tz;
-							double scaled_rz = scale*dz/0.015;
+//							double total_std = sqrt(total_cov);
+//							double scale = 1.0/total_std;
+							double dz = dst_z-src_tz;
+//							double scaled_rz = scale*dz/0.015;
 
-							double contribution_weight = 1.0-(dst_info/(src_info+dst_info));
+							double scaled_rz = dz*dz/(0.01*0.01*total_cov);
+
+							double contribution_weight = dst_cov/(src_cov+dst_cov);
 							double reliability_weight = dr*sr;
-							double overlap_prob = exp(-0.5*scaled_rz*scaled_rz);
+							double overlap_prob = fabs(scaled_rz) < 1.0;//exp(-0.5*scaled_rz*scaled_rz);
 							double total_weight = overlap_prob*reliability_weight*contribution_weight;
-
-							addConstraint(dst_w,dst_h,dst_z,src_tz, total_weight);
-
-
-//							rzdata[3*src_ind+0] = 255.0 * exp(-0.5*scaled_rz*scaled_rz);
-//							rzdata[3*src_ind+1] = rzdata[3*src_ind+0];
-//							rzdata[3*src_ind+2] = rzdata[3*src_ind+0];
-
-							if(scaled_rz >= 1){
-								rzdata[3*src_ind+0] = 0;
-								rzdata[3*src_ind+1] = 0;
-								rzdata[3*src_ind+2] = 0;
-							}if(scaled_rz <= -1){
-								rzdata[3*src_ind+0] = 0;
-								rzdata[3*src_ind+1] = 0;
-								rzdata[3*src_ind+2] = 0;
-							}else{
-
-								rzdata[3*src_ind+0] = 0;
-								rzdata[3*src_ind+1] = 255.0 * std::min(1.0,std::max(0.0,(0.5 + 0.25*scaled_rz)))	 * reliability_weight * contribution_weight;
-								rzdata[3*src_ind+2] = 255.0 * std::min(1.0,std::max(0.0,(0.5 + 0.25*(1-scaled_rz)))) * reliability_weight * contribution_weight;
+							if(total_weight > 0.001){
+								addConstraint(dst_w/float(dst_width),dst_h/float(dst_height),dst_z,src_tz, total_weight);
 							}
 
                         }
@@ -190,49 +350,129 @@ public:
         }
 
 
-
-
-
-		cv::namedWindow( "rz"	, cv::WINDOW_AUTOSIZE );
-		cv::imshow( "rz",	rz );
-
-		cv::namedWindow( "src_reliability"	, cv::WINDOW_AUTOSIZE );
-		cv::imshow( "src_reliability",	src_reliability );
-
-		cv::namedWindow( "dst_reliability"	, cv::WINDOW_AUTOSIZE );
-		cv::imshow( "dst_reliability",	dst_reliability );
-
-        cv::waitKey(0);
-
-//        if(visualizationLvl == 1){
-//            viewer->removeAllPointClouds();
-//            pcl::PointCloud<pcl::PointXYZRGB>::Ptr src_cld = src->getSmallPCLcloud();
-//            pcl::PointCloud<pcl::PointXYZRGB>::Ptr dst_cld = dst->getSmallPCLcloud();
-
-//            for(unsigned int i = 0; i < src_cld->points.size(); i++){
-//                src_cld->points[i].r = 255;
-//                src_cld->points[i].g = 0;
-//                src_cld->points[i].b = 0;
-//            }
-
-//            for(unsigned int i = 0; i < dst_cld->points.size(); i++){
-//                dst_cld->points[i].r = 0;
-//                dst_cld->points[i].g = 255;
-//                dst_cld->points[i].b = 0;
-//            }
-
-//            pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZRGB> ());
-//            pcl::transformPointCloud (*src_cld, *transformed_cloud, p);
-
-//            viewer->addPointCloud<pcl::PointXYZRGB> (transformed_cloud, pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>(transformed_cloud), "src_cloud");
-//            viewer->addPointCloud<pcl::PointXYZRGB> (dst_cld, pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>(dst_cld), "dst_cloud");
-//            viewer->spin();
-//        }
+		double stop2 = reglib::getTime();
+		//printf("time: %5.5fs %5.5fs\n",stop1-start1,stop2-start2);
 
     }
 
+	virtual void print(){}
+	virtual void shrink(){}
+};
+
+class CameraOptimizerGridXY : public CameraOptimizer{
+public:
+    int grid_w;
+    int grid_h;
+    std::vector<double> mul;
+    std::vector<double> sum;
+
+	int getInd(int w, int h){return h*grid_w+w;}
+
+    CameraOptimizerGridXY(int gw = 6, int gh = 4, double bias = 100){
+        grid_w = gw;
+        grid_h = gh;
+		int nr_corners = (1+grid_w)*(1+grid_h);
+		mul.resize(nr_corners);
+		sum.resize(nr_corners);
+		for(unsigned long i = 0; i < nr_corners;i++){
+            sum[i] = bias;
+            mul[i] = sum[i];
+        }
+        visualizationLvl = 0;
+    }
+
+    ~CameraOptimizerGridXY(){}
+
+	virtual void shrink(){
+		for(unsigned long i = 0; i < mul.size();i++){
+			mul[i] *= 0.5;
+			sum[i] *= 0.5;
+
+			if(sum[i] < 100){
+				mul[i] = 100.0*mul[i]/sum[i];
+				sum[i] = 100.0;
+			}
+		}
+	}
+
+    virtual void addConstraint(double w, double h, double z, double z2, double weight){
+        double ratio = z2/z;
+
+		double wg = double(grid_w)*w;
+		double hg = double(grid_h)*h;
+		//printf("%f %f -> %f %f\n",w,h,wg,hg);
+		double dw = wg-std::floor(wg);
+		double dh = hg-std::floor(hg);
+		int ind00 = getInd(wg  , hg  );//int(wg) + int(hg)*grid_w;
+		int ind01 = getInd(wg  , hg+1);//int(wg) + int(hg+1)*grid_w;
+		int ind10 = getInd(wg+1, hg  );//int(wg+1) + int(hg)*grid_w;
+		int ind11 = getInd(wg+1, hg+1);//int(wg+1) + int(hg+1)*grid_w;
+		double mul00 = (1-dw)*(1-dh);
+		double mul01 = (1-dw)*(dh);
+		double mul10 = (dw)*(1-dh);
+		double mul11 = (dw)*(dh);
 
 
+
+		sum[ind00] +=       weight*mul00;
+		mul[ind00] += ratio*weight*mul00;
+
+		sum[ind01] +=       weight*mul01;
+		mul[ind01] += ratio*weight*mul01;
+
+		sum[ind10] +=       weight*mul10;
+		mul[ind10] += ratio*weight*mul10;
+
+		sum[ind11] +=       weight*mul11;
+		mul[ind11] += ratio*weight*mul11;
+    }
+
+    virtual double getRange(double w, double h, double z){
+
+		//return z;
+
+        double wg = double(grid_w)*w;
+        double hg = double(grid_h)*h;
+		//printf("%f %f -> %f %f\n",w,h,wg,hg);
+        double dw = wg-std::floor(wg);
+        double dh = hg-std::floor(hg);
+		int ind00 = getInd(wg  , hg  );//int(wg) + int(hg)*grid_w;
+		int ind01 = getInd(wg  , hg+1);//int(wg) + int(hg+1)*grid_w;
+		int ind10 = getInd(wg+1, hg  );//int(wg+1) + int(hg)*grid_w;
+		int ind11 = getInd(wg+1, hg+1);//int(wg+1) + int(hg+1)*grid_w;
+        double mul00 = (1-dw)*(1-dh);
+        double mul01 = (1-dw)*(dh);
+        double mul10 = (dw)*(1-dh);
+		double mul11 = (dw)*(dh);
+
+		double r00 = mul[ind00]/sum[ind00];
+		double r01 = mul[ind01]/sum[ind01];
+		double r10 = mul[ind10]/sum[ind10];
+		double r11 = mul[ind11]/sum[ind11];
+		double ratio = (mul00*r00 + mul01*r01  + mul10*r10 + mul11*r11);
+        return z*ratio;
+    }
+
+
+	virtual void print(){
+		for(unsigned int w = 0; w < grid_w; w++){
+			for(unsigned int h = 0; h < grid_h; h++){
+				unsigned int ind = h * grid_w + w;
+				printf("%i %i -> %5.5f / %5.5f -> %5.5f\n",w,h,mul[ind],sum[ind],mul[ind]/sum[ind]);
+			}
+		}
+	}
+
+	virtual void normalize(){
+		double totsum = 0;
+		for(unsigned long i = 0; i < mul.size();i++){
+			totsum += mul[i]/sum[i];
+		}
+		double mean = totsum / double(mul.size());
+		for(unsigned long i = 0; i < mul.size();i++){
+			mul[i] /= mean;
+		}
+	}
 };
 
 
@@ -246,15 +486,34 @@ void train_cam(reglib::Model * model){
 
 
 
-    CameraOptimizer * co = new CameraOptimizer();
+	CameraOptimizerGridXY * co = new CameraOptimizerGridXY(640/5,480/5);
     co->setVisualization(viewer,1);
-    for(unsigned int i = 0; i < model->frames.size(); i++){
-        for(unsigned int j = 0; j < model->frames.size(); j++){
-            if(i == j){continue;}
-            Eigen::Matrix4d m = model->relativeposes[j].inverse() * model->relativeposes[i];
-            co->addTrainingData( model->frames[i], model->frames[j], m);//model->relativeposes[i] * model->relativeposes[j].inverse());
-        }
-    }
+	for(int it = 0; it < 3; it++){
+		for(unsigned int i = 0; i < model->frames.size(); i++){
+			printf("%i / %i\n",i+1,model->frames.size());
+			for(unsigned int j = 0; j < model->frames.size(); j++){
+				if(i == j){continue;}
+				Eigen::Matrix4d m = model->relativeposes[j].inverse() * model->relativeposes[i];
+				co->addTrainingData( model->frames[i], model->frames[j], m);//model->relativeposes[i] * model->relativeposes[j].inverse());
+				co->normalize();
+
+			}
+			co->show(false);
+			co->normalize();
+		}
+		co->print();
+		co->show(true);
+		co->shrink();
+	}
+
+	for(unsigned int i = 0; i < model->frames.size(); i++){
+		for(unsigned int j = 0; j < model->frames.size(); j++){
+			if(i == j){continue;}
+			Eigen::Matrix4d m = model->relativeposes[j].inverse() * model->relativeposes[i];
+			co->show( model->frames[i], model->frames[j], m);//model->relativeposes[i] * model->relativeposes[j].inverse());
+		}
+	}
+	exit(0);
 }
 
 int main(int argc, char **argv){
